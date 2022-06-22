08073b3396a7bbae4b4160e102f05fdf0addf2c8
Tampoka <goal.thanks@gmail.com>
2022-06-22 16:54:23 +0900

    Add getCode.

--- src/auth/ClientProvider.tsx
+++ src/auth/ClientProvider.tsx
@@ -2,27 +2,28 @@ import {FC, PropsWithChildren, useEffect, useState} from 'react';
 import ApolloClient from "apollo-boost"
 import * as keytar from "keytar"
 import {ApolloProvider} from "react-apollo-hooks";
+import {getCode} from './getCode';
 
 const GITHUB_BASE_URL = "https://api.github.com/graphql"
 
-export const ClientProvider: FC<PropsWithChildren<{}>>= ({
-         children
-     }) => {
+export const ClientProvider: FC<PropsWithChildren> = ({
+                                                          children
+                                                      }) => {
     const [token, setToken] = useState<string>()
 
-    useEffect(()=>{
-        const getToken=async()=>{
-            let key:any=await keytar.getPassword(
+    useEffect(() => {
+        const getToken = async () => {
+            let key: any = await keytar.getPassword(
                 "github",
                 process.env.CLIENT_ID!
             )
-            if(!key){
-                key=await getCode()
+            if (!key) {
+                key = await getCode()
             }
             setToken(key)
         }
         getToken()
-    },[])
+    }, [])
 
     if (!token) {
         return <>Loading...</>
---
+++ src/auth/getCode.ts
@@ -0,0 +1,17 @@
+import * as http from 'http';
+import * as url from 'url';
+
+export const getCode=()=>{
+    return new Promise((resolve,reject)=>{
+        http
+            .createServer(function (req,res) {
+                if(!req.url){
+                    return
+                }
+                const {code}=url.parse(req.url,true).query
+                res.writeHead(200, { "Content-Type": "text/plain" })
+                res.write(`The code is: ${code}`)
+                res.end()
+            })
+    })
+}
