e86800039f922b5d9fe141ee626cc4e954aa3ac7
Tampoka <goal.thanks@gmail.com>
2022-06-22 16:55:44 +0900

    Add PORT.

--- src/auth/getCode.ts
+++ src/auth/getCode.ts
@@ -1,17 +1,20 @@
 import * as http from 'http';
 import * as url from 'url';
 
-export const getCode=()=>{
-    return new Promise((resolve,reject)=>{
+const PORT = 3000
+
+export const getCode = () => {
+    return new Promise((resolve, reject) => {
         http
-            .createServer(function (req,res) {
-                if(!req.url){
+            .createServer(function (req, res) {
+                if (!req.url) {
                     return
                 }
-                const {code}=url.parse(req.url,true).query
-                res.writeHead(200, { "Content-Type": "text/plain" })
+                const {code} = url.parse(req.url, true).query
+                res.writeHead(200, {"Content-Type": "text/plain"})
                 res.write(`The code is: ${code}`)
                 res.end()
             })
+            .listen(PORT)
     })
 }
