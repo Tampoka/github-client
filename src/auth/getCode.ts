b9deb406edbb8d1a35c3c9d503bd84a952cfa057
Tampoka <goal.thanks@gmail.com>
2022-06-22 17:00:58 +0900

    Add FormData.

--- src/auth/getCode.ts
+++ src/auth/getCode.ts
@@ -1,5 +1,9 @@
 import * as http from 'http';
 import * as url from 'url';
+import * as keytar from "keytar";
+
+const FomData = require("form-data");
+const open = require("open")
 
 const PORT = 3000
 
@@ -13,8 +17,37 @@ export const getCode = () => {
                 const {code} = url.parse(req.url, true).query
                 res.writeHead(200, {"Content-Type": "text/plain"})
                 res.write(`The code is: ${code}`)
-                res.end()
+                res.end();
+
+                (async () => {
+                    const data = new FormData();
+                    data.append("client_id", process.env.CLIENT_ID!);
+                    data.append("client_secret", process.env.CLIENT_SECRET!);
+                    data.append("code", `${code}`);
+                    data.append("state", "abc");
+                    data.append("redirect_uri", "http://localhost:3000");
+
+                    fetch("https://github.com/login/oauth/access_token", {
+                        method: "POST",
+                        body: data,
+                        headers: {
+                            Accept: "application/json",
+                        },
+                    })
+                        .then((res: any) => res.json())
+                        .then(async (data: any) => {
+                            await keytar.setPassword(
+                                "github",
+                                process.env.CLIENT_ID!,
+                                data.access_token
+                            );
+                            resolve(data.access_token);
+                        });
+                })();
             })
-            .listen(PORT)
-    })
-}
+            .listen(PORT);
+        open(
+            `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user%20read:org%20public_repo%20admin:enterprise&state=abc`
+        );
+    });
+};
