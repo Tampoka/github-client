7e17986bb517ffd2870d224b5e7ec8c3e4c5c73d
Tampoka <goal.thanks@gmail.com>
2022-06-22 16:48:31 +0900

    Add ClientProvider.

---
+++ src/auth/ClientProvider.tsx
@@ -0,0 +1,43 @@
+import {FC, PropsWithChildren, useEffect, useState} from 'react';
+import ApolloClient from "apollo-boost"
+import * as keytar from "keytar"
+import {ApolloProvider} from "react-apollo-hooks";
+
+const GITHUB_BASE_URL = "https://api.github.com/graphql"
+
+export const ClientProvider: FC<PropsWithChildren<{}>>= ({
+         children
+     }) => {
+    const [token, setToken] = useState<string>()
+
+    useEffect(()=>{
+        const getToken=async()=>{
+            let key:any=await keytar.getPassword(
+                "github",
+                process.env.CLIENT_ID!
+            )
+            if(!key){
+                key=await getCode()
+            }
+            setToken(key)
+        }
+        getToken()
+    },[])
+
+    if (!token) {
+        return <>Loading...</>
+    }
+
+    const client = new ApolloClient({
+        uri: GITHUB_BASE_URL,
+        request: (operation) => {
+            operation.setContext({
+                headers: {
+                    authorization: `Bearer ${token}`
+                }
+            })
+        }
+    })
+
+    return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
